b86ffa0f221106dc7e596e698b02fb334d6551d7
Kshitiz Shrestha <kshitizshrestha12345@gmail.com>
2024-09-15 00:57:30 +0545

    todo-model-and-url

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from todo_api import urls as todo_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("todos", include(todo_urls))
 ]
